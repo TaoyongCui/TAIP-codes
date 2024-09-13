85986145871b02b436a2488360eb4398a7f43f19
TaoyongCui <cty21@tsinghua.org.cn>
2024-09-13 12:30:43 +0800

    xyz2pt

--- xyz2pt.py
+++ xyz2pt.py
@@ -14,55 +14,49 @@ from torch_geometric.nn import radius_graph
 import numpy as np
 from ase.atoms import Atoms
 from ase.io import read
-import pdb
+
 
 
 
 if __name__ == '__main__':
     file2=argv[2]
-    # import os
-    # import pdb
-    # path = r'./'
-    # file1 = os.listdir(path)
+    import os
+    import pdb
+    path = r'./'
+    file1 = os.listdir(path)
     atoms_all_list = []
-    # for files in file1:
-    atoms_all = read(argv[1], index=slice(0, 5032), format='extxyz')
-    atoms_all_list.append(atoms_all)
-
+    for files in file1:
+        atoms_all = read(argv[1], index=slice(None), format='extxyz')#[:100]
+        atoms_all_list.append(atoms_all)
+#
     data_list =[]
     for i in range(len(atoms_all_list)):
         for i, atoms in enumerate(atoms_all_list[i]):
-            force=torch.as_tensor(atoms.get_forces(), dtype=torch.float64),
-            # energy=torch.as_tensor(atoms.get_potential_energy(), dtype=torch.float64)
-            atoms.wrap() #wrap the atomic positions according to cells
             natom = len(atoms)
             pos_arr = atoms.positions
-            # energy = atoms.
+
             pos = pos_arr.tolist()
-            # pbc_index = list(range(natom))
-            # pbc_image = [[0, 0, 0] for _ in range(natom)]
+            pbc_index = list(range(natom))
+            pbc_image = [[0, 0, 0] for _ in range(natom)]
             atoms_numbers = atoms.numbers.tolist()
-            # nbr_dist = [[[i, 0.0]] for i in range(natom)]
+            nbr_dist = [[[i, 0.0]] for i in range(natom)]
 
             type_idx = []
             type_map = {'H': 0, 'C': 1, 'N': 2, 'O': 3, 'F': 4, 'Na': 5, 'P': 6}
             x = torch.as_tensor(atoms.numbers, dtype=torch.int)
             cell = torch.as_tensor(atoms.cell[:])
-
-
-
+            pos=torch.as_tensor(atoms.positions, dtype=torch.float)
+            dist = (pos[row] - pos[col]).norm(dim=-1)
 
             data = Data(
                 pos=pos,
                 z=x,
-                # y=energy,
-                # energy=energy,
-                force=force[0],
-                cell=torch.as_tensor(atoms.get_cell(), dtype=torch.float64),
+                energy=torch.as_tensor(atoms.get_potential_energy(), dtype=torch.float),
+                force=torch.as_tensor(atoms.get_forces(), dtype=torch.float),
                 natoms=len(atoms.numbers),
+                cell=cell
             )
             data_list.append(data)
 
 
     torch.save(data_list, file2)
-
